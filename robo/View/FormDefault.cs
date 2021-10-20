a695bec6491131f7e1b0dc9a33a0d90d1355aaee
Emanuel <devsantos1999@gmail.com>
2021-10-20 16:11:54 -0300

    Insert item for Aditamento Legado

--- robo/View/FormDefault.cs
+++ robo/View/FormDefault.cs
@@ -8,17 +8,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static robo.View.FormInterface2;
 
 namespace robo.View
 {
     public partial class FormDefault : Form
     {
         List<Point> pontosComboBox = new List<Point>() { new Point(20, 140), new Point(20, 224) };
-        int posicaoLabel, posicaoCombo = 0;
-        public FormDefault(TOMenus menuSelecionado, string tipoFies)
+        public FormDefault(TOMenus menuSelecionado, string tipoFies, string tipoAditamento)
         {
             InitializeComponent();
-
             LimparForm();
             foreach (System.Windows.Forms.Control item in flowLayoutPanel1.Controls)
             {
@@ -27,16 +26,27 @@ namespace robo.View
                     if (item.Tag.ToString().Contains(menuSelecionado.Item))
                     {
                         item.Visible = true;
-                        //if (item.GetType() == typeof(GroupBox))
-                        //{
-                        //    item.Location = pontosComboBox[posicaoCombo];
-                        //    posicaoCombo++;
-                        //}
+                        string resultado = Convert.ToString(item.Tag);
+                        if (resultado == "ADITAMENTO")
+                        {
+                            if (tipoAditamento == "FIES LEGADO")
+                            {
+                                foreach (System.Windows.Forms.Control item2 in flowLayoutPanel1.Controls)
+                                {
+                                    if (item2.Tag != null)
+                                    {
+                                        if (item2.Tag.ToString().Contains("A_LEGADO"))
+                                        {
+                                            item2.Visible = true;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-
         private void LimparForm()
         {
             foreach (System.Windows.Forms.Control control in flowLayoutPanel1.Controls)
