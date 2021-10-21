cc365e305c40a22715bafa3decbb70745089df71
Emanuel <devsantos1999@gmail.com>
2021-10-21 17:52:10 -0300

    Arrastar Painel

--- robo/View/FormInterface2.cs
+++ robo/View/FormInterface2.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,12 @@ namespace robo.View
     public partial class FormInterface2 : Form
     {
         FormDefault formulario;
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
         public FormInterface2()
         {
             InitializeComponent();
@@ -140,6 +147,15 @@ namespace robo.View
         private void btnVoltar_Click(object sender, EventArgs e)
         {
             panelCadastro.BringToFront();
+        }
+
+        private void panelMenuBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
         }
     }
 }
