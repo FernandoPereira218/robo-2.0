f52437dd82ceda5a84e87b062dce04c708e47b63
Fernando <fernandofmp218@gmail.com>
2021-11-28 17:27:49 -0300

    Refatoração e correções UsuarioForm e LoginForm

--- robo/Interface/UsuarioForm.cs
+++ robo/Interface/UsuarioForm.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using robo.Utils;
 using System.Windows.Forms;
 using robo.Banco_de_Dados;
+using System.Collections.Generic;
 
 namespace robo.Interface
 {
@@ -14,33 +15,83 @@ namespace robo.Interface
         {
             InitializeComponent();
             this.Location = location;
-            cbIES.Text = Program.login.IES.ToUpper();
-            cbIES.Enabled = false;
+            PreencherCbIES();
+            PreencherCbRegional();
             if (usuario != null)
             {
-                txtId.Text = usuario.Id.ToString();
-                txtUser.Text = usuario.Usuario;
-                txtSenhaUsuario.Text = string.Empty;
-                cbPermissoes.Text = usuario.Permissao;
-                cbRegional.Text = usuario.Regional;
-
-                this.btnOKLogin.Text = "Atualizar";
-                txtId.Enabled = false;
-                this.txtUser.Enabled = false;
-                this.btnOKLogin.Click -= new EventHandler(this.btnOKLogin_Click);
-                this.btnOKLogin.Click += new EventHandler(this.btnAtualizarLogin_Click);
+                InicializarAtualizarUsuario(usuario);
             }
             else
             {
-                txtUser.Text = string.Empty;
-                txtSenhaUsuario.Text = string.Empty;
-                cbPermissoes.Text = string.Empty;
-
-                this.btnOKLogin.Text = "Aceitar";
-                this.txtUser.Enabled = true;
-                this.btnOKLogin.Click -= new EventHandler(this.btnAtualizarLogin_Click);
-                this.btnOKLogin.Click += new EventHandler(this.btnOKLogin_Click);
+                InicializarNovoUsuario();
             }
+        }
+
+        private void PreencherCbIES()
+        {
+            if (Program.login.Usuario != "Admin")
+            {
+                List<TOUsuario> usuarios = Dados.SelectWhere<TOUsuario>(x => x.Regional == Program.login.Regional);
+                cbIES.DataSource = PreencherLista(usuarios, nameof(TOUsuario.IES));
+            }
+            else
+            {
+                List<TOUsuario> usuarios = Dados.SelectAll<TOUsuario>();
+                cbIES.DataSource = PreencherLista(usuarios, nameof(TOUsuario.IES));
+            }
+        }
+
+        private void PreencherCbRegional()
+        {
+            if (Program.login.Usuario != "Admin")
+            {
+                cbRegional.Text = Program.login.Regional;
+                cbRegional.Enabled = false;
+            }
+            else
+            {
+                List<TOUsuario> usuarios = Dados.SelectAll<TOUsuario>();
+                cbRegional.DataSource = PreencherLista(usuarios, nameof(TOUsuario.Regional));
+            }
+        }
+
+        private List<string> PreencherLista(List<TOUsuario> usuarios, string nomePropriedade)
+        {
+            List<string> lista = new List<string>();
+            foreach (TOUsuario usuario in usuarios)
+            {
+                string valorPropriedade = (string)usuario.GetType().GetProperty(nomePropriedade).GetValue(usuario);
+                if (lista.Contains(valorPropriedade) == false)
+                {
+                    lista.Add(valorPropriedade);
+                }
+            }
+            return lista;
+        }
+
+        private void InicializarAtualizarUsuario(TOUsuario usuario)
+        {
+            txtId.Text = usuario.Id.ToString();
+            txtUser.Text = usuario.Usuario;
+            txtSenhaUsuario.Text = string.Empty;
+            cbPermissoes.Text = usuario.Permissao;
+            cbRegional.Text = usuario.Regional;
+
+            this.btnOKLogin.Text = "Atualizar";
+            this.txtId.Enabled = false;
+            this.txtUser.Enabled = false;
+            this.cbRegional.Enabled = false;
+            this.btnOKLogin.Click -= btnNovoUsuario_Click;
+            this.btnOKLogin.Click += btnAtualizarUsuario_Click;
+        }
+
+        private void InicializarNovoUsuario()
+        {
+            this.txtId.Text = Convert.ToString(Dados.Count<TOUsuario>() + 1);
+            this.txtId.Enabled = false;
+            this.btnOKLogin.Text = "Aceitar";
+            this.btnOKLogin.Click -= btnAtualizarUsuario_Click;
+            this.btnOKLogin.Click += btnNovoUsuario_Click;
         }
 
         private void btnCancelLogin_Click(object sender, EventArgs e)
@@ -48,7 +99,7 @@ namespace robo.Interface
             this.Close();
         }
 
-        private void btnOKLogin_Click(object sender, EventArgs e)
+        private void btnNovoUsuario_Click(object sender, EventArgs e)
         {
             try
             {
@@ -65,7 +116,7 @@ namespace robo.Interface
             }
         }
 
-        private void btnAtualizarLogin_Click(object sender, EventArgs e)
+        private void btnAtualizarUsuario_Click(object sender, EventArgs e)
         {
             try
             {
@@ -84,13 +135,15 @@ namespace robo.Interface
 
         private TOUsuario UsuarioPreenchido()
         {
-            TOUsuario Usuario = new TOUsuario();
-            Usuario.Id = Convert.ToInt32(txtId.Text);
-            Usuario.Usuario = txtUser.Text;
-            Usuario.Senha = Util.GetMD5(txtSenhaUsuario.Text);
-            Usuario.Permissao = cbPermissoes.Text;
-            Usuario.IES = cbIES.Text;
-            Usuario.Regional = cbRegional.Text;
+            TOUsuario Usuario = new TOUsuario()
+            {
+                Id = Convert.ToInt32(txtId.Text),
+                Usuario = txtUser.Text,
+                Senha = Util.GetMD5(txtSenhaUsuario.Text),
+                Permissao = cbPermissoes.Text,
+                IES = cbIES.Text,
+                Regional = cbRegional.Text
+            };
             return Usuario;
         }
     }
